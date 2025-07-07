e954f1a739a87b5292a4757192511c3871531d21
omarFaruk <2035faruk@gmail.com>
2025-07-07 14:11:25 +0600

    initialize project setup

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+"use client"
+import React, {useRef} from 'react';
+import PageHeading from "@/components/PageHeading";
+import {Toast} from "primereact/toast";
 
 const HomePage = () => {
+    const  toast = useRef(null);
+
     return (
         <div className="card">
-            Start work here
+            <Toast ref={toast}/>
+           <PageHeading title={"Home"}/>
         </div>
     );
 };
